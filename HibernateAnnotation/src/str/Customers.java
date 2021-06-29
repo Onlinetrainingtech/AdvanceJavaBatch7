c4f31ee56c4f598b9ca2850290ae1ddab09c0055
OnlineTrainer <mohamedtechdata@gmail.com>
2021-06-29 07:46:25 +0530

    29-06-2021

---
+++ HibernateAnnotation/src/str/Customers.java
@@ -0,0 +1,30 @@
+package str;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Customers 
+{
+    @Id
+    @Column(name="custid")
+    private int customerid;
+    @Column(name="custname",length=10)
+    private String customername;
+	public int getCustomerid() {
+		return customerid;
+	}
+	public void setCustomerid(int customerid) {
+		this.customerid = customerid;
+	}
+	public String getCustomername() {
+		return customername;
+	}
+	public void setCustomername(String customername) {
+		this.customername = customername;
+	}
+    
+}
